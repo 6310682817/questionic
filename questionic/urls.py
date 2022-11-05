c71929c601d449d4c2d6a3351b1e6733fbb83449
Paisan Saeloe <6310682809@cn102>
2022-11-05 22:35:22 +0700

    post_question

--- questionic/urls.py
+++ questionic/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'questionic'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-]
+    
+    path('post_question/', views.post_question, name='post_question'),
+    # path('question/<int:question_id>', views.question, name='question'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- questionic/views.py
+++ questionic/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import Question, Account, QuestionFile, Answer, AnswerFile, ReplyAnswer, ReplyAnswerFile
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -9,3 +11,22 @@ def index(request):
 
 def about(request):
     return render(request, 'questionic/about.html')
+
+def post_question(request):
+    if request.method == 'POST':
+        user = User.objects.get(username=request.user.username)
+
+        # tags = request.POST['Tags']
+        title = request.POST['Title']
+        detail = request.POST['Detail']
+        category = request.POST['Category']
+        grade = request.POST['Grade']
+        asker = Account.objects.get(user=user)
+        images = request.FILES.getlist('images')
+
+        question=Question.objects.create(title = title, detail = detail,  category=category, grade=grade, asker=asker)
+
+        for img in images:
+            QuestionFile.objects.create(question=question, image=img)
+        # return HttpResponseRedirect(reverse('questionic:question', args=(question.id, )))    
+    return render(request, 'questionic/post_question.html')
