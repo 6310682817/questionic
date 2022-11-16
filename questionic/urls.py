7849b97cf6256fe32facd805c832a670005dc287
Nattawut Pichaikan <6310682817@cn330>
2022-11-16 21:40:24 +0700

    set path media

--- questionic/urls.py
+++ questionic/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
-
+from django.urls import path, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 app_name = 'questionic'
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,6 +13,7 @@ urlpatterns = [
     path('notification_alert/', views.notification_alert, name='notification_alert'),
     path('search/', views.search, name='search'),
     path('report/<str:type_report>/<int:report_id>', views.report, name='report'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
